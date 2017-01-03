ead520fe183a5a84bc23a9b9051a0937fba85ef7
Shugo Maeda <shugo@ruby-lang.org>
2017-01-03 13:17:50 +0900

    Add key map.

--- lib/text_bringer/controller.rb
+++ lib/text_bringer/controller.rb
@@ -10,6 +10,8 @@ module TextBringer
       @buffer = TextBringer::Buffer.new
       @window = nil
       @key_sequence =[]
+      @key_map = {}
+      setup_keys
     end
 
     def start(args)
@@ -24,7 +26,7 @@ module TextBringer
         @window = TextBringer::Window.new(@buffer,
                                           Curses.lines - 1, Curses.cols, 0, 0)
         @status_window = Curses::Window.new(1, Curses.cols, Curses.lines - 1, 0)
-        @status_message = @status_window << "Quit by ESC"
+        @status_message = @status_window << "Quit by C-x C-c"
         @status_window.refresh
         @window.redisplay
         command_loop
@@ -34,6 +36,45 @@ module TextBringer
       end
     end
 
+    private
+
+    def set_key(key, &block)
+      @key_map[kbd(key)] = block
+    end
+
+    def kbd(key)
+      case key
+      when Integer
+        [key]
+      when String
+        key.unpack("C*")
+      else
+        raise TypeError, "invalid key type #{key.class}"
+      end
+    end
+
+    def setup_keys
+      set_key("\C-x\C-c") { exit }
+      set_key(Curses::KEY_RIGHT) { @buffer.forward_char }
+      set_key(?\C-f) { @buffer.forward_char }
+      set_key(Curses::KEY_LEFT) { @buffer.backward_char }
+      set_key(?\C-b) { @buffer.backward_char }
+      set_key(Curses::KEY_DOWN) { @buffer.next_line }
+      set_key(?\C-n) { @buffer.next_line }
+      set_key(Curses::KEY_UP) { @buffer.previous_line }
+      set_key(?\C-p) { @buffer.previous_line }
+      set_key(Curses::KEY_DC) { @buffer.delete_char }
+      set_key(?\C-d) { @buffer.delete_char }
+      set_key(Curses::KEY_BACKSPACE) { @buffer.backward_delete_char }
+      set_key(?\C-h) { @buffer.backward_delete_char }
+      set_key(?\C-a) { @buffer.beginning_of_line }
+      set_key(?\C-e) { @buffer.end_of_line }
+      (0x20..0x7e).each do |c|
+        set_key(c) { @buffer.insert(c.chr) }
+      end
+      set_key(?\n) { @buffer.insert("\n") }
+    end
+
     def command_loop
       while c = @window.getch
         if @status_message
@@ -41,43 +82,22 @@ module TextBringer
           @status_window.refresh
           @status_message = nil
         end
+        @key_sequence << c.ord
+        cmd = @key_map[@key_sequence]
         begin
-          case c
-          when ?\e.ord
-            exit
-          when Curses::KEY_RIGHT, ?\C-f.ord
-            @buffer.forward_char
-          when Curses::KEY_LEFT, ?\C-b.ord
-            @buffer.backward_char
-          when Curses::KEY_DOWN, ?\C-n.ord
-            @buffer.next_line
-          when Curses::KEY_UP, ?\C-p.ord
-            @buffer.previous_line
-          when Curses::KEY_DC, ?\C-d.ord
-            @buffer.delete_char
-          when Curses::KEY_BACKSPACE, ?\C-h.ord
-            @buffer.backward_delete_char
-          when ?\C-a.ord
-            @buffer.beginning_of_line
-          when ?\C-e.ord
-            @buffer.end_of_line
-          when String
-            @buffer.insert(c)
-          when ?\n.ord
-            @buffer.insert(?\n)
+          if cmd
+            cmd.call
+            @key_sequence = []
           else
-            if c < 128
-              @buffer.insert(c.chr)
-            elsif c < 256
-              @key_sequence << c
-              s = @key_sequence.pack("C*").force_encoding("utf-8")
-              if s.valid_encoding?
-                @buffer.insert(s)
-                @key_sequence = []
-              end
-            else
-              @status_message = @status_window << "KEY:#{c.inspect}"
+            if @key_sequence.all? { |c| 0x80 <= c && c <= 0xff } &&
+                (s = @key_sequence.pack("C*").force_encoding("utf-8")).valid_encoding?
+              @buffer.insert(s)
+              @key_sequence = []
+            elsif @key_sequence[0] != ?\C-x.ord || @key_sequence.size > 1
+              keys = @key_sequence.map { |c| Curses.keyname(c) }.join(" ")
+              @status_message = @status_window << "#{keys} is undefined"
               @status_window.refresh
+              @key_sequence = []
             end
           end
         rescue => e
