87dbc77a02eff652c71437f98604b6d7ae60e29c
Shugo Maeda <shugo@ruby-lang.org>
2017-01-28 23:10:06 +0900

    Add a prefix argument to find_tag.

--- lib/textbringer/commands/ctags.rb
+++ lib/textbringer/commands/ctags.rb
@@ -6,10 +6,84 @@ module Textbringer
 
     CTAGS = {
       path: nil,
-      tags: nil
+      tags: nil,
+      name: nil,
+      candidates: nil,
+      index: nil
     }
 
-    define_command(:find_tag) do
+    define_command(:find_tag) do |next_p = current_prefix_arg|
+      prev_file = nil
+      tags = get_tags
+      if next_p
+        name = CTAGS[:name]
+        if name.nil?
+          raise EditorError, "Tag search not started yet"
+        end
+        candidates = CTAGS[:candidates]
+        index = CTAGS[:index]
+        prev_file, = candidates[index]
+        if next_p == :-
+          index -= 1
+        else
+          index += 1
+        end
+        if index < 0
+          raise EditorError, "No previous tags for #{name}"
+        end
+        if index >= candidates.size
+          raise EditorError, "No more tags for #{name}"
+        end
+      else
+        buffer = Buffer.current
+        name = buffer.save_excursion {
+          buffer.backward_word(regexp: /[A-Za-z_\-]/)
+          if buffer.looking_at?(/[A-Za-z_\-]+/)
+            match_string(0)
+          else
+            nil
+          end
+        }
+        if name.nil?
+          raise EditorError, "No name found at point"
+        end
+        candidates = tags[name]
+        if candidates.empty?
+          raise EditorError, "Tag not found: #{name}"
+        end
+        CTAGS[:name] = name
+        CTAGS[:candidates] = candidates
+        CTAGS[:index] = index = 0
+      end
+      file, addr = candidates[index]
+      find_file(file)
+      case addr
+      when /\A\d+\z/
+        goto_line(addr.to_i)
+      when %r'\A/\^(.*)\$/\z'
+        if file == prev_file
+          if next_p == :-
+            beginning_of_line
+          else
+            end_of_line
+          end
+        else
+          beginning_of_buffer
+        end
+        re = "^" + Regexp.quote($1) + "$"
+        if next_p == :-
+          re_search_backward(re)
+        else
+          re_search_forward(re)
+        end
+        beginning_of_line
+      else
+        raise EditorError, "Invalid address: #{addr}"
+      end
+      Window.current.recenter_if_needed
+    end
+
+    def get_tags
       path = File.expand_path("tags")
       if CTAGS[:path] != path
         CTAGS[:path] = path
@@ -20,37 +94,8 @@ module Textbringer
         end
         CTAGS[:tags] = tags
         message("Loaded #{path}")
-      else
-        tags = CTAGS[:tags]
       end
-      buffer = Buffer.current
-      name = buffer.save_excursion {
-        buffer.backward_word(regexp: /[A-Za-z_\-]/)
-        if buffer.looking_at?(/[A-Za-z_\-]+/)
-          match_string(0)
-        else
-          nil
-        end
-      }
-      if name.nil?
-        raise EditorError, "No name found at point"
-      end
-      candidates = tags[name]
-      if candidates.empty?
-        raise EditorError, "Tag not found: #{name}"
-      end
-      file, addr = candidates.first
-      find_file(file)
-      case addr
-      when /\A\d+\z/
-        goto_line(addr.to_i)
-      when %r'\A/\^(.*)\$/\z'
-        beginning_of_buffer        
-        re_search_forward("^" + Regexp.quote($1) + "$")
-      else
-        raise EditorError, "Invalid address: #{addr}"
-      end
-      Window.current.recenter_if_needed
+      CTAGS[:tags]
     end
   end
 end
