9265402774650d414e03cc7dd6e9c434a9536d10
Shugo Maeda <shugo@ruby-lang.org>
2017-02-02 00:06:42 +0900

    Add tests for ctags.

---
+++ test/textbringer/test_ctags.rb
@@ -0,0 +1,74 @@
+require_relative "../test_helper"
+
+class TestCtags < Textbringer::TestCase
+  def test_find_tag
+    pwd = Dir.pwd
+    Dir.chdir(File.expand_path("../fixtures/ctags", __dir__))
+    begin
+      assert_raise(EditorError) do
+        find_tag(true)
+      end
+      find_file("test.rb")
+      re_search_forward(/^foo/)
+      find_tag
+      assert_equal("foo.rb", Buffer.current.name)
+      assert_equal(2, Buffer.current.current_line)
+      assert_equal(1, Buffer.current.current_column)
+      find_tag(true)
+      assert_equal("test.rb", Buffer.current.name)
+      assert_equal(1, Buffer.current.current_line)
+      assert_equal(1, Buffer.current.current_column)
+      re_search_forward(/^bar/)
+      pos = Buffer.current.point
+      find_tag
+      assert_equal("foo.rb", Buffer.current.name)
+      assert_equal(6, Buffer.current.current_line)
+      assert_equal(1, Buffer.current.current_column)
+      pop_tag_mark
+      assert_equal("test.rb", Buffer.current.name)
+      assert_equal(pos, Buffer.current.point)
+      re_search_forward(/^baz/)
+      find_tag
+      assert_equal("foo.rb", Buffer.current.name)
+      assert_equal(10, Buffer.current.current_line)
+      assert_equal(1, Buffer.current.current_column)
+      pop_tag_mark
+      re_search_backward(/^foo/)
+      find_tag
+      assert_equal("foo.rb", Buffer.current.name)
+      assert_equal(2, Buffer.current.current_line)
+      assert_equal(1, Buffer.current.current_column)
+      find_tag(true)
+      assert_equal("test.rb", Buffer.current.name)
+      assert_equal(1, Buffer.current.current_line)
+      assert_equal(1, Buffer.current.current_column)
+      assert_raise(EditorError) do
+        find_tag(true)
+      end
+      find_tag(:-)
+      assert_equal("foo.rb", Buffer.current.name)
+      assert_equal(2, Buffer.current.current_line)
+      assert_equal(1, Buffer.current.current_column)
+      assert_raise(EditorError) do
+        find_tag(:-)
+      end
+      pop_tag_mark
+      re_search_forward(/quux/)
+      assert_raise(EditorError) do
+        find_tag
+      end
+      end_of_buffer
+      assert_raise(EditorError) do
+        find_tag
+      end
+      4.times do
+        pop_tag_mark
+      end
+      assert_raise(EditorError) do
+        pop_tag_mark
+      end
+    ensure
+      Dir.chdir(pwd)
+    end
+  end
+end
