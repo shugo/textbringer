6d4d3bb3d8f1e089d21b74eb4e30f27ece26570e
Shugo Maeda <shugo@ruby-lang.org>
2017-02-03 14:43:16 +0900

    Add tests for commands/replace.

--- test/textbringer/commands/test_replace.rb
+++ test/textbringer/commands/test_replace.rb
@@ -16,7 +16,24 @@ class TestReplace < Textbringer::TestCase
 　茶がいかに貴重か、高価か、また地方にもまだない物かは、彼もよくわきまえていた。
 EOF
     buffer.beginning_of_buffer
-    push_keys("yn.")
+    push_keys("yq")
+    query_replace_regexp("茶", "水")
+    assert_equal("茶", match_string(0))
+    assert_equal(<<EOF, buffer.to_s)
+　劉備は、船の商人らしい男を見かけてあわててそばへ寄って行った。
+「水を売って下さい、茶が欲しいんですが」
+「え、茶だって？」
+　洛陽《らくよう》の商人は、鷹揚《おうよう》に彼を振向いた。
+「あいにくと、お前さんに頒《わ》けてやるような安茶は持たないよ。一葉《ひとは》いくらというような佳品しか船にはないよ」
+「結構です。たくさんは要《い》りませんが」
+「おまえ茶をのんだことがあるのかね。地方の衆が何か葉を煮てのんでいるが、あれは茶ではないよ」
+「はい。その、ほんとの茶を頒《わ》けていただきたいのです」
+　彼の声は、懸命だった。
+　茶がいかに貴重か、高価か、また地方にもまだない物かは、彼もよくわきまえていた。
+EOF
+
+    buffer.beginning_of_buffer
+    push_keys("n.")
     query_replace_regexp("茶", "水")
     assert_equal(<<EOF, buffer.to_s)
 　劉備は、船の商人らしい男を見かけてあわててそばへ寄って行った。
