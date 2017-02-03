ce4767182cd103f2a5d66ff4e88bde414a7e507f
Shugo Maeda <shugo@ruby-lang.org>
2017-02-03 14:40:08 +0900

    Add test_mode.rb.

---
+++ test/textbringer/test_mode.rb
@@ -0,0 +1,34 @@
+require_relative "../test_helper"
+
+class TestMode < Textbringer::TestCase
+  class FooMode < Mode
+    define_generic_command :foo
+  end
+
+  class BarMode < FooMode
+    def foo
+      :ok
+    end
+  end
+
+  class BazMode < FooMode
+    def foo
+      self.no_such_method
+    end
+  end
+  
+  def test_generic_command
+    foo_mode
+    assert_raise(EditorError) do
+      foo_command
+    end
+
+    bar_mode
+    assert_equal(:ok, foo_command)
+
+    baz_mode
+    assert_raise(NoMethodError) do
+      foo_command
+    end
+ end
+end
