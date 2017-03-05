377218894a31820f12cc6426c70df9da4b59f424
Shugo Maeda <shugo@ruby-lang.org>
2017-03-05 18:44:55 +0900

    Highligh symbols.g

--- lib/textbringer/modes/ruby_mode.rb
+++ lib/textbringer/modes/ruby_mode.rb
@@ -68,6 +68,10 @@ module Textbringer
             )
             \k<heredoc_quote>
             (?> (?:.|\n)*? \k<heredoc_terminator> )
+      ) |
+      (?:
+        : [_a-zA-Z\u{0100}-\u{10ffff}]
+            [_a-zA-Z0-9\u{0100}-\u{10ffff}]*
       )
     /x
 
