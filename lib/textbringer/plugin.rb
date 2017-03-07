688fd17a8dee5e43aea93e49628fbd02af65e59d
Shugo Maeda <shugo@ruby-lang.org>
2017-03-07 18:01:58 +0900

    Load only latest plugins.

--- lib/textbringer/plugin.rb
+++ lib/textbringer/plugin.rb
@@ -4,7 +4,15 @@ module Textbringer
   module Plugin
     def self.load_plugins
       files = Gem.find_files("textbringer_plugin.rb")
-      files.each do |file|
+      files.group_by { |file|
+        file.slice(/([^\/]+)-[\w.]+\/lib\/textbringer_plugin\.rb\z/, 1)
+      }.map { |gem, versions|
+        versions.sort_by { |version|
+          v = version.slice(/[^\/]+-([\w.]+)\/lib\/textbringer_plugin\.rb\z/,
+                            1)
+          Gem::Version.create(v)
+        }.last
+      }.each do |file|
         load(file)
       end
     end
---
+++ test/textbringer/test_plugin.rb
@@ -0,0 +1,47 @@
+require_relative "../test_helper"
+
+class TestMode < Textbringer::TestCase
+  module FindFilesExt
+    @@files = nil
+
+    def self.files=(files)
+      @@files = files
+    end
+
+    def find_files(*args)
+      @@files || super(*args)
+    end
+  end
+
+  Gem.singleton_class.prepend(FindFilesExt)
+
+  teardown do
+    FindFilesExt.files = nil
+  end
+
+  def test_load_plugins
+    Dir.mktmpdir do |dir|
+      FileUtils.mkdir_p(File.expand_path("foo-0.9.0/lib", dir))
+      foo_0_9_0 = File.expand_path("foo-0.9.0/lib/textbringer_plugin.rb", dir)
+      File.write(foo_0_9_0, <<~EOF)
+        $FOO_VERSION = "0.9.0"
+      EOF
+      FileUtils.mkdir_p(File.expand_path("foo-0.10.0/lib", dir))
+      foo_0_10_0 =
+        File.expand_path("foo-0.10.0/lib/textbringer_plugin.rb", dir)
+      File.write(foo_0_10_0, <<~EOF)
+        $FOO_VERSION = "0.10.0"
+      EOF
+      FileUtils.mkdir_p(File.expand_path("bar-0.1.0/lib", dir))
+      bar_0_1_0 = File.expand_path("bar-0.1.0/lib/textbringer_plugin.rb", dir)
+      File.write(bar_0_1_0, <<~EOF)
+        $BAR_VERSION = "0.1.0"
+      EOF
+
+      FindFilesExt.files = [foo_0_10_0, foo_0_9_0, bar_0_1_0]
+      Plugin.load_plugins
+      assert_equal("0.10.0", $FOO_VERSION)
+      assert_equal("0.1.0", $BAR_VERSION)
+    end
+  end
+end
