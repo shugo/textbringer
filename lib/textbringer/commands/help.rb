7da37639178824a3ec29e0e64ccd6969accc5a85
Shugo Maeda <shugo@ruby-lang.org>
2017-03-11 08:29:54 +0900

    Add space.

--- lib/textbringer/commands/help.rb
+++ lib/textbringer/commands/help.rb
@@ -5,8 +5,8 @@ module Textbringer
     define_command(:describe_bindings) do
       help = Buffer.find_or_new("*Help*", undo_limit: 0)
       help.read_only_edit do
-        s = format("%-16s%s\n", "Key", "Binding")
-        s << format("%-16s%s\n", "---", "-------")
+        s = format("%-16s  %s\n", "Key", "Binding")
+        s << format("%-16s  %s\n", "---", "-------")
         s << "\n"
         bindings = {}
         [
@@ -19,7 +19,7 @@ module Textbringer
           end
         end
         bindings.each do |key_sequence, command|
-          s << format("%-16s%s\n",
+          s << format("%-16s  %s\n",
                       Keymap.key_sequence_string(key_sequence),
                       command)
         end
