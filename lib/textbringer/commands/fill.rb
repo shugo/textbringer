e69463d07592325103d4e7909c87a191a985af98
Shugo Maeda <shugo@ruby-lang.org>
2017-07-11 08:02:33 +0900

    Fill English text correctly.

--- lib/textbringer/commands/fill.rb
+++ lib/textbringer/commands/fill.rb
@@ -16,22 +16,39 @@ module Textbringer
         input = StringIO.new(str)
         output = String.new
         fill_column = CONFIG[:fill_column]
+        prev_c = nil
         while c = input.getc
           if column < fill_column && c == "\n"
+            if /\w/ =~ prev_c
+              next_c = input.getc
+              input.ungetc(next_c)
+              if /\w/ =~ next_c
+                output << " "
+                column += 1
+              end
+            end
             next
           end
-          w = Buffer.display_width(c)
           if c == "\n"
             output << c
             column = 0
           else
+            w = Buffer.display_width(c)
             if column + w > fill_column || column >= fill_column
-              output << "\n"
-              column = 0
+              if /\w/ =~ prev_c && /\w/ =~ c
+                if output.sub!(/([^\w\n])(\w*)\z/, "\\1\n")
+                  output << $2
+                  column = Buffer.display_width($2)
+                end
+              else
+                output << "\n"
+                column = 0
+              end
             end
             output << c
             column += w
           end
+          prev_c = c
         end
         buffer.composite_edit do
           buffer.delete_region(s, e)
