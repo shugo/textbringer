f1888ea380b688ccdb7bddbd8c7c316f4b074d5c
Shugo Maeda <shugo@ruby-lang.org>
2021-02-05 11:49:22 +0900

    Support 55

--- lib/textbringer/input_methods/t_code_input_method.rb
+++ lib/textbringer/input_methods/t_code_input_method.rb
@@ -24,9 +24,11 @@ module Textbringer
         c = KANJI_TABLE[key_index][@prev_key_index]
         @prev_key_index = nil
         case c
-        when "■"
+        when ?■
           nil
-        when "◆"
+        when ?⑤
+          show_stroke
+        when ?◆
           bushu_compose
         else
           c
@@ -49,6 +51,43 @@ module Textbringer
           nil
         end
       end
+    end
+
+
+    def show_stroke
+      c = Buffer.current.char_after
+      x, y = KANJI_TABLE.find.with_index { |row, i|
+        j = row.index(c)
+        if j
+          break [j, i]
+        else
+          false
+        end
+      }
+      s = "　" * 10 + "・・・・　　・・・・" * 3
+      s[x] = "１"
+      s[y] = "２"
+      message = s.gsub(/.{10}/, "\\&\n").gsub(/　/, "  ")
+      show_help(message)
+      Window.redisplay
+      nil
+    end
+
+    def show_help(message)
+      buffer = Buffer.find_or_new("*T-Code Help*",
+                                  undo_limit: 0, read_only: true)
+      buffer.read_only_edit do
+        buffer.clear
+        buffer.insert(message)
+        buffer.beginning_of_buffer
+      end
+      if Window.list.size == 1
+        split_window
+        # shrink_window(Window.current.lines - 8)
+      end
+      windows = Window.list
+      i = (windows.index(Window.current) + 1) % windows.size
+      windows[i].buffer = buffer
     end
   end
 end
