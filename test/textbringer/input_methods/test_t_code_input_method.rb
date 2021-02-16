d66d79fc00edbce571988288a4766eea5094f7ba
Shugo Maeda <shugo@ruby-lang.org>
2021-02-16 16:56:16 +0900

    Add more tests

--- test/textbringer/input_methods/test_t_code_input_method.rb
+++ test/textbringer/input_methods/test_t_code_input_method.rb
@@ -8,6 +8,10 @@ class TestTCodeInputMethod < Textbringer::TestCase
     switch_to_buffer(@buffer)
   end
 
+  def test_insert_direct
+    assert_equal(?`, @im.handle_event(?`))
+  end
+
   def test_insert_kanji
     assert_equal(nil, @im.handle_event(?z))
     assert_equal(?字, @im.handle_event(?/))
@@ -54,5 +58,53 @@ EOF
     assert_equal("△漢字", @buffer.to_s)
     assert_equal(nil, @im.handle_event(?\C-m))
     assert_equal("漢字", @buffer.to_s)
+  end
+
+  def test_mazegaki_conversion_with_inflection
+    @buffer.insert("おもう")
+    assert_equal(nil, @im.handle_event(?5))
+    assert_equal(nil, @im.handle_event(?8))
+    assert_equal("△おもう", @buffer.to_s)
+    assert_equal(nil, @im.handle_event(?d))
+    assert_equal("思う", @buffer.to_s)
+  end
+
+  def test_relimit_right
+    @buffer.insert("おもう")
+    assert_equal(nil, @im.handle_event(?f))
+    assert_equal(nil, @im.handle_event(?j))
+    assert_equal("お△もう", @buffer.to_s)
+    assert_equal(nil, @im.handle_event(?>))
+    assert_equal("おも△う", @buffer.to_s)
+    assert_equal(nil, @im.handle_event(?>))
+    assert_equal("△おもう", @buffer.to_s)
+    assert_equal(nil, @im.handle_event(?d))
+    assert_equal("思う", @buffer.to_s)
+  end
+
+  def test_relimit_left
+    @buffer.insert("きかんしゃ")
+    assert_equal(nil, @im.handle_event(?f))
+    assert_equal(nil, @im.handle_event(?j))
+    assert_equal("△機関車", @buffer.to_s)
+    assert_equal(nil, @im.handle_event(?>))
+    assert_equal("き△{官舎,感謝}", @buffer.to_s)
+    assert_equal(nil, @im.handle_event(?<))
+    assert_equal("△機関車", @buffer.to_s)
+    assert_equal(nil, @im.handle_event(?\C-m))
+    assert_equal("機関車", @buffer.to_s)
+  end
+
+  def test_show_stroke
+    @buffer.insert("漢字")
+    @buffer.beginning_of_buffer
+    assert_equal(nil, @im.handle_event(?5))
+    assert_equal(nil, @im.handle_event(?5))
+    assert_equal(<<EOF, Buffer["*T-Code Help*"].to_s)
+      ２            
+・・・・    ・・・・
+・・・・    ・・１・
+・・・・    ・・・・
+EOF
   end
 end
