406d504a72a61b51130b26bc821488c71e4b4a31
Shugo Maeda <shugo@ruby-lang.org>
2021-02-17 12:33:12 +0900

    Add a test for read_input_method_name

--- test/textbringer/commands/test_input_method.rb
+++ test/textbringer/commands/test_input_method.rb
@@ -13,5 +13,25 @@ class TestInputMethodCommands < Textbringer::TestCase
     toggle_input_method("t_code")
     assert_instance_of(TCodeInputMethod, Buffer.current.input_method)
     assert(Buffer.current.input_method.enabled?)
+    Controller.current.current_prefix_arg = [4]
+    push_keys("hira\t\n")
+    toggle_input_method
+    assert_instance_of(HiraganaInputMethod, Buffer.current.input_method)
+    assert(Buffer.current.input_method.enabled?)
+  end
+
+  def test_read_input_method_name
+    push_keys("\n")
+    s = read_input_method_name("Input method: ")
+    assert_equal("t_code", s)
+
+    push_keys("hira\t\n")
+    s = read_input_method_name("Input method: ")
+    assert_equal("hiragana", s)
+
+    push_keys("katakana\t\n")
+    s = read_input_method_name("Input method: ")
+    assert_equal("katakana", s)
   end
 end
+
