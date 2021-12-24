36a47d5afcc21849ff44c9cfc51b76fde309a51c
Shugo Maeda <shugo@ruby-lang.org>
2021-12-24 11:06:59 +0900

    Skip UNIX server tests on Windows

--- test/textbringer/commands/test_server.rb
+++ test/textbringer/commands/test_server.rb
@@ -15,38 +15,42 @@ class TestServer < Textbringer::TestCase
   end
 
   def test_visit_file
-    server_start
-    Tempfile.create do |f|
-      t = Thread.start {
-        tb = DRbObject.new_with_uri(CONFIG[:server_uri])
-        tb.visit_file(f.path)
-      } 
-      Controller.current.call_next_block
-      assert_equal(f.path, Buffer.current.file_name)
-      insert("Hello, world!")
-      save_buffer
-      server_edit_done
-      assert_equal("Hello, world!", f.read)
-      assert_equal(:done, t.value)
+    omit_on_windows do
+      server_start
+      Tempfile.create do |f|
+        t = Thread.start {
+          tb = DRbObject.new_with_uri(CONFIG[:server_uri])
+          tb.visit_file(f.path)
+        } 
+        Controller.current.call_next_block
+        assert_equal(f.path, Buffer.current.file_name)
+        insert("Hello, world!")
+        save_buffer
+        server_edit_done
+        assert_equal("Hello, world!", f.read)
+        assert_equal(:done, t.value)
+      ensure
+        server_kill
+      end
     end
-  ensure
-    server_kill
   end
 
   def test_eval
-    foo = Buffer.new_buffer("foo")
-    switch_to_buffer(foo)
-    server_start
-    Tempfile.create do |f|
-      t = Thread.start {
-        tb = DRbObject.new_with_uri(CONFIG[:server_uri])
-        tb.eval('Buffer.current.name')
-      } 
-      Controller.current.call_next_block
-      assert_equal('"foo"', t.value)
+    omit_on_windows do
+      foo = Buffer.new_buffer("foo")
+      switch_to_buffer(foo)
+      server_start
+      Tempfile.create do |f|
+        t = Thread.start {
+          tb = DRbObject.new_with_uri(CONFIG[:server_uri])
+          tb.eval('Buffer.current.name')
+        } 
+        Controller.current.call_next_block
+        assert_equal('"foo"', t.value)
+      end
+    ensure
+      server_kill
     end
-  ensure
-    server_kill
   end
 
   def test_server_sock_mode_no_options
@@ -58,18 +62,22 @@ class TestServer < Textbringer::TestCase
   end
 
   def test_server_sock_mode_empty_options
-    CONFIG[:server_options] = {}
-    server_start
-    assert_equal(0600, File.stat(@sock_path).mode & 0777)
-  ensure
-    server_kill
+    omit_on_windows do
+      CONFIG[:server_options] = {}
+      server_start
+      assert_equal(0600, File.stat(@sock_path).mode & 0777)
+    ensure
+      server_kill
+    end
   end
 
   def test_server_sock_mode_specified_options
-    CONFIG[:server_options] = { UNIXFileMode: 0700 }
-    server_start
-    assert_equal(0700, File.stat(@sock_path).mode & 0777)
-  ensure
-    server_kill
+    omit_on_windows do
+      CONFIG[:server_options] = { UNIXFileMode: 0700 }
+      server_start
+      assert_equal(0700, File.stat(@sock_path).mode & 0777)
+    ensure
+      server_kill
+    end
   end
 end
