9e5d27c1bd625d2e48c2af6d290081e24dc2cd37
Shugo Maeda <shugo@ruby-lang.org>
2025-05-10 10:02:19 +0900

    Add test_overwrite_mode.rb

---
+++ test/textbringer/modes/test_overwrite_mode.rb
@@ -0,0 +1,58 @@
+require_relative "../../test_helper"
+require "tmpdir"
+
+class TestOverwriteMode < Textbringer::TestCase
+  setup do
+    @buffer = Buffer.new_buffer("overwrite_test")
+    switch_to_buffer(@buffer)
+  end
+
+  def test_overwrite_mode
+    insert("abcdefg\n")
+    insert("あいうえお\n")
+    beginning_of_buffer
+    Controller.current.last_key = "x"
+    overwrite_mode
+    self_insert
+    assert_equal(<<~EOF, Buffer.current.to_s)
+      xbcdefg
+      あいうえお
+    EOF
+    forward_char(2)
+    Controller.current.last_key = "y"
+    self_insert
+    assert_equal(<<~EOF, Buffer.current.to_s)
+      xbcyefg
+      あいうえお
+    EOF
+    Controller.current.last_key = "か"
+    self_insert
+    assert_equal(<<~EOF, Buffer.current.to_s)
+      xbcyかfg
+      あいうえお
+    EOF
+    next_line
+    Controller.current.last_key = "き"
+    self_insert
+    assert_equal(<<~EOF, Buffer.current.to_s)
+      xbcyかfg
+      あいうきお
+    EOF
+    Controller.current.current_prefix_arg = [3]
+    Controller.current.last_key = "z"
+    self_insert
+    assert_equal(<<~EOF.chomp, Buffer.current.to_s)
+      xbcyかfg
+      あいうきzzz
+    EOF
+    overwrite_mode
+    beginning_of_buffer
+    Controller.current.current_prefix_arg = nil
+    Controller.current.last_key = "q"
+    self_insert
+    assert_equal(<<~EOF.chomp, Buffer.current.to_s)
+      qxbcyかfg
+      あいうきzzz
+    EOF
+  end
+end
