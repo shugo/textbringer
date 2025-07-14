0ee83f4195fcf45fae6b2d92b2d480829f2a1cc4
Shugo Maeda <shugo@ruby-lang.org>
2025-07-14 17:48:16 +0900

    Support interactive commands in ispell_buffer

--- lib/textbringer/commands/ispell.rb
+++ lib/textbringer/commands/ispell.rb
@@ -17,7 +17,7 @@ module Textbringer
         case result
         when /\A&\s+([^\s]+)\s+\d+\s+\d+:\s+(.*)/
           [$1, $2.split(/, /)]
-        when /\A\*/, /\A\+/
+        when /\A\*/, /\A\+/, /\A\-/, /\A\%/
           [word, []]
         when /\A#/
           [word, nil]
@@ -26,12 +26,40 @@ module Textbringer
         end
       end
 
+      def add_to_session_dictionary(word)
+        @stdin.puts("@" + word)
+        @stdin.flush
+      end
+
+      def add_to_personal_dictionary(word)
+        @stdin.puts("*" + word)
+        @stdin.flush
+      end
+
       def close
         @stdin.close
         @stdout.close
         @stderr.close
       end
     end
+
+    define_keymap :ISPELL_MODE_MAP
+    (?\x20..?\x7e).each do |c|
+      ISPELL_MODE_MAP.define_key(c, :ispell_unknown_command)
+    end
+    ISPELL_MODE_MAP.define_key(?\t, :ispell_unknown_command)
+    ISPELL_MODE_MAP.handle_undefined_key do |key|
+      ispell_unknown_command
+    end
+    ISPELL_MODE_MAP.define_key(?r, :ispell_replace)
+    ISPELL_MODE_MAP.define_key(?a, :ispell_accept)
+    ISPELL_MODE_MAP.define_key(?i, :ispell_insert)
+    ISPELL_MODE_MAP.define_key(" ", :ispell_skip)
+    ISPELL_MODE_MAP.define_key(?q, :ispell_quit)
+
+    ISPELL_STATUS = {}
+
+    ISPELL_WORD_REGEXP = /[A-Za-z]+/
 
     define_command(:ispell_word) do
       buffer = Buffer.current
@@ -70,41 +98,96 @@ module Textbringer
       end
     end
 
-    define_command(:ispell_buffer) do
+    define_command(:ispell_buffer) do |recursive_edit: false|
+      Buffer.current.beginning_of_buffer
+      ispell_mode
+      ispell_forward
+      ISPELL_STATUS[:recursive_edit] = recursive_edit
+      if recursive_edit
+        recursive_edit()
+      end
+    end
+
+    def ispell_done
+      Buffer.current.delete_visible_mark
+      Controller.current.overriding_map = nil
+      ISPELL_STATUS[:ispell]&.close
+      ISPELL_STATUS[:ispell] = nil
+      if ISPELL_STATUS[:recursive_edit]
+        exit_recursive_edit
+      end
+    end
+
+    def ispell_mode
+      ISPELL_STATUS[:ispell] = Ispell.new
+      Controller.current.overriding_map = ISPELL_MODE_MAP
+    end
+
+    def ispell_forward
       buffer = Buffer.current
-      ispell = Ispell.new
-      begin
-        buffer.save_excursion do
-          buffer.beginning_of_buffer
-          while buffer.re_search_forward(/[A-Za-z]+/, raise_error: false)
-            word = buffer.match_string(0)
-            _original, suggestions = ispell.check_word(word)
-            next if suggestions.nil? || suggestions.empty?
-            buffer.save_excursion do
-              buffer.goto_char(buffer.match_beginning(0))
-              buffer.set_visible_mark
-            end
-            recenter
-            Window.redisplay
-            s = read_from_minibuffer("Correct #{word} with: ",
-                                     completion_proc: ->(s) {
-                                       suggestions.grep(/^#{Regexp.quote(s)}/)
-                                     })
-            if !s.empty?
-              pos = buffer.point
-              buffer.backward_word
-              buffer.composite_edit do
-                buffer.delete_region(buffer.point, pos)
-                buffer.insert(s)
-              end
-            end
-          end
+      while buffer.re_search_forward(ISPELL_WORD_REGEXP, raise_error: false,
+                                     goto_beginning: true)
+        ispell_beginning = buffer.point
+        buffer.set_visible_mark
+        buffer.goto_char(buffer.match_end(0))
+        word = buffer.match_string(0)
+        _original, suggestions = ISPELL_STATUS[:ispell].check_word(word)
+        if !suggestions.nil? && !suggestions.empty?
+          ISPELL_STATUS[:beginning] = ispell_beginning
+          ISPELL_STATUS[:word] = word
+          ISPELL_STATUS[:suggestions] = suggestions
+          message("Mispelled: #{word}  [r]eplace, [a]ccept, [i]nsert, [SPC] to skip, [q]uit")
+          recenter
+          return
         end
-      ensure
-        ispell.close
-        Buffer.current.delete_visible_mark
       end
       message("Finished spelling check.")
+      ispell_done
+    end
+
+    define_command(:ispell_replace) do
+      word = ISPELL_STATUS[:word]
+      suggestions = ISPELL_STATUS[:suggestions]
+      s = read_from_minibuffer("Correct #{word} with: ",
+                               completion_proc: ->(s) {
+        suggestions.grep(/^#{Regexp.quote(s)}/)
+      })
+      if !s.empty?
+        buffer = Buffer.current
+        pos = buffer.point
+        buffer.goto_char(ISPELL_STATUS[:beginning])
+        buffer.composite_edit do
+          buffer.delete_region(buffer.point, pos)
+          buffer.insert(s)
+        end
+      end
+      ispell_forward
+    end
+
+    define_command(:ispell_accept) do
+      ISPELL_STATUS[:ispell].add_to_session_dictionary(ISPELL_STATUS[:word])
+      ispell_forward
+    end
+
+    define_command(:ispell_insert) do
+      ISPELL_STATUS[:ispell].add_to_personal_dictionary(ISPELL_STATUS[:word])
+      ispell_forward
+    end
+
+    define_command(:ispell_skip) do
+      ISPELL_STATUS[:ispell].add_to_personal_dictionary(ISPELL_STATUS[:word])
+      ispell_forward
+    end
+
+    define_command(:ispell_quit) do
+      message("Quitting spell check.")
+      ispell_done
+    end
+
+    define_command(:ispell_unknown_command) do
+      word = ISPELL_STATUS[:word]
+      message("Mispelled: #{word}  [r]eplace, [a]ccept, [i]nsert, [SPC] to skip, [q]uit")
+      Window.beep
     end
   end
 end
--- test/textbringer/commands/test_ispell.rb
+++ test/textbringer/commands/test_ispell.rb
@@ -37,17 +37,44 @@ class TestIspell < Textbringer::TestCase
   def test_ispell_buffer
     insert("helllo world\nthis is a pen.")
     goto_char(0)
-    push_keys("hello\n")
-    ispell_buffer
+    push_keys("rhello\n")
+    ispell_buffer(recursive_edit: true)
     assert_equal("hello world\nthis is a pen.", Buffer.current.to_s)
+    assert_equal("Finished spelling check.", Window.echo_area.message)
+  end
+
+  def test_ispell_buffer_last_word
+    insert("hello world\nthis is a penn")
+    goto_char(0)
+    push_keys("rpen\n")
+    ispell_buffer(recursive_edit: true)
+    assert_equal("hello world\nthis is a pen", Buffer.current.to_s)
     assert_equal("Finished spelling check.", Window.echo_area.message)
   end
 
   def test_ispell_buffer_not_modified
     insert("helllo world\nthis is a pen.")
     goto_char(0)
-    push_keys("\n")
-    ispell_buffer
+    push_keys("q")
+    ispell_buffer(recursive_edit: true)
+    assert_equal("helllo world\nthis is a pen.", Buffer.current.to_s)
+    assert_equal("Quitting spell check.", Window.echo_area.message)
+  end
+
+  def test_ispell_buffer_accept
+    insert("helllo world\nthis is a pen.")
+    goto_char(0)
+    push_keys("a")
+    ispell_buffer(recursive_edit: true)
+    assert_equal("helllo world\nthis is a pen.", Buffer.current.to_s)
+    assert_equal("Finished spelling check.", Window.echo_area.message)
+  end
+
+  def test_ispell_buffer_insert
+    insert("helllo world\nthis is a pen.")
+    goto_char(0)
+    push_keys("i")
+    ispell_buffer(recursive_edit: true)
     assert_equal("helllo world\nthis is a pen.", Buffer.current.to_s)
     assert_equal("Finished spelling check.", Window.echo_area.message)
   end
