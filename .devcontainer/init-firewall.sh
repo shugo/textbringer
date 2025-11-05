fde4fae52a7e03a550b96c6b4bed448d25c1e30e
Shugo Maeda <shugo@ruby-lang.org>
2025-11-05 11:48:35 +0900

    Add ruby to DevContainer

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -72,7 +72,8 @@ for domain in \
     "statsig.com" \
     "marketplace.visualstudio.com" \
     "vscode.blob.core.windows.net" \
-    "update.code.visualstudio.com"; do
+    "update.code.visualstudio.com" \
+    "rubygems.org"; do
     echo "Resolving $domain..."
     ips=$(dig +noall +answer A "$domain" | awk '$4 == "A" {print $5}')
     if [ -z "$ips" ]; then
