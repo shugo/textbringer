89842ed72eea2fd61d310dc8690adb05f743a94f
Shugo Maeda <shugo@ruby-lang.org>
2025-11-10 14:23:29 +0900

    Specify the foreground color of the region face

--- lib/textbringer/faces/basic.rb
+++ lib/textbringer/faces/basic.rb
@@ -2,6 +2,6 @@ module Textbringer
   Face.define :mode_line, reverse: true
   Face.define :link, foreground: "blue", bold: true
   Face.define :control
-  Face.define :region, background: "blue"
+  Face.define :region, background: "blue", foreground: "white"
   Face.define :isearch, background: "yellow", foreground: "black"
 end
